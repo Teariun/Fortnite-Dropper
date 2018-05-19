78a648ff7c786f69c74e37e39c9626bae2106aec
niklaseeeee <niklas5559@outlook.de>
2018-05-19 21:30:38 +0200

    Version 1.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,22 @@
 // Koordinaten der Städte
-var xKoord = ["113", "290", "447","605","384","105","675","542","763","573","831","744","240","189","898","829", "362"]; 
-var yKoord = ["237", "321", "388","487","510","471","325","235","551","645","301","223","629","129","445","792", "900"];
+//var xKoord = ["113", "290", "447","605","384","105","675","542","763","573","831","744","240","189","898","829", "362"]; 
+//var yKoord = ["237", "321", "388","487","510","471","325","235","551","645","301","223","629","129","445","792", "900"];
 
+var coordinates = [
+    [445, 250, "Loot Lake"]
+];
 
-var anzahlStaedte = xKoord.length;
+var offset = [-45, 55]
+
+var anzahlStaedte = coordinates.length;
 
 function randomize(){
     // Cursor sichtbar machen
-    document.getElementById('cursor').style.visibility = 'visible';
-
+    $('#cursor').css('visibility', 'visible');
+    console.log('visible!')
 	// Zufallsstadt
-    var zufallsZahl = Math.floor((Math.random() * anzahlStaedte)); // Zufallszahl von allen Kordinaten
-    console.log(zufallsZahl)
-
-	document.getElementById('cursor').style.left= xKoord[zufallsZahl]-42 + "px";
-    document.getElementById('cursor').style.top = yKoord[zufallsZahl]-75 + "px";
+    var randomNumber = Math.floor((Math.random() * anzahlStaedte)); // randomNumber von allen Kordinaten
+    console.log(coordinates[randomNumber][0] + offset[0])
+    $('#cursor').css('left',(coordinates[randomNumber][0] + offset[0]) + 'px');
+    $('#cursor').css('top', (coordinates[randomNumber][1] + offset[1]) + 'px');
 }
